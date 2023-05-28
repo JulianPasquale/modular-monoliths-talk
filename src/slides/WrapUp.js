dfe073c42a0194798f478a3a38a76ac947657a28
Julian Pasquale <julianagustin78@gmail.com>
2023-05-28 11:10:24 -0300

    Add community and wrap-up

---
+++ src/slides/WrapUp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function WrapUp() {
+  return (
+    <section>
+      <section>
+        <h3>Preguntas?</h3>
+      </section>
+      <section>
+        <h3>Gracias!</h3>
+      </section>
+    </section>
+  );
+}
+
+export default WrapUp;
--- src/slides/index.js
+++ src/slides/index.js
@@ -2,3 +2,5 @@ export { default as CoverPage } from './CoverPage';
 export { default as Agenda } from './Agenda';
 export { default as WhyModularize } from './WhyModularize';
 export { default as MCApproach } from './MCApproach';
+export { default as Community } from './Community';
+export { default as WrapUp } from './WrapUp';
